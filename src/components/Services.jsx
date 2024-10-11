708950b5b930497c301e9ce173b3bad9c0aa13fe
GideonCove <gideoncove@gmail.com>
2024-10-11 15:47:47 -0400

    updating services page

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,7 +7,18 @@ import {Navigate, useNavigate, useParams } from 'react-router-dom'
 export default function Services () {
     return(
         <div className="Services">
-            <h1> 4C is owned by Kevin Cove</h1>
+            <ul>
+                <li>Project Management Support or Review Services for:
+                    <ul>
+                        <li>Project Approvals</li>
+                        <li>Proposals</li>
+                        <li>Contracts</li>
+                    </ul>
+                </li>
+                <li>Site visits/project audits</li>
+                <li>Civil / Structural support both site and engineering</li>
+                <li>Engineering Management</li>
+            </ul>
         </div>
     )
 }
