af7886b6ea11ded3be82ae4c2664d35f8a058343
GideonCove <gideoncove@gmail.com>
2024-10-11 14:49:56 -0400

    Home page updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,9 @@ import {Navigate, useNavigate, useParams } from 'react-router-dom'
 export default function Home () {
     return (
         <div className="Home">
-            <p> 4C consulting</p>
+            <p> 4See Consulting</p>
+            <p>A Cement Consultant Company. </p>
+            <p> Founded in 2023, lead with decades of experience working on the biggest cement projects in North America.</p>
 
         </div>
     )
