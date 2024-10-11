450ca692d7ffca09f161d00b21d846d9af352dd2
GideonCove <gideoncove@gmail.com>
2024-10-11 14:59:33 -0400

    services page added and about page updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,15 @@
 import axios from "axios"
 import { useState, useEffect} from "react"
 import {Navigate, useNavigate, useParams } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
 
 
 
 export default function About () {
     return(
-        <div className="ABout">
-            <h1> 4C is owned by Kevin Cove</h1>
+        <div className="About">
+            <h1> 4See Consulting</h1><p> was founded by owner Kevin Cove. Kevin has decades of experience working on cement projects around the world, from Vietnam to Mitchell, Indiana.</p>
+            <Button variant="info" href=""
         </div>
     )
 }
---
+++ src/components/Services.jsx
@@ -0,0 +1,13 @@
+import axios from "axios"
+import { useState, useEffect} from "react"
+import {Navigate, useNavigate, useParams } from 'react-router-dom'
+
+
+
+export default function Services () {
+    return(
+        <div className="Services">
+            <h1> 4C is owned by Kevin Cove</h1>
+        </div>
+    )
+}
