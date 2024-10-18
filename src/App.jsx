abe76feab02d5048c6d57f056ea62c1d7c21e15b
GideonCove <gideoncove@gmail.com>
2024-10-17 22:45:20 -0400

    fixed nax

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Footer from './components/Footer'
 import axios from 'axios'
 import {Route, Routes} from 'react-router-dom'
 import Main from './components/Main'
+import Nav from './components/Nav'
 
 export default function App () {
   return (
     <div className="App">
       <p> App</p>
       <Header />
+      <Nav /> 
       <Main />
       <Footer />
     </div>
