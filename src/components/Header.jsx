991f085ba1464622dd3c9700f7da93dfaabc2ed4
GideonCove <gideoncove@gmail.com>
2024-10-25 15:14:17 -0400

    adding images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import {Navigate, useNavigate, useParams } from 'react-router-dom'
 export default function Header (){
     return (
         <div className="Header">
-           
+           <img src ="Pics/IMG_2325 - Copy.JPG" alt="Mitchell Plant Preheater at Night"></img>
         </div>
     )
 }
