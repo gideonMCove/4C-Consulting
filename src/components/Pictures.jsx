ea2440319742a07cb629a1fb450a71e34bd212e1
GideonCove <gideoncove@gmail.com>
2024-10-28 11:00:53 -0400

    adding pictures to pictures tab

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -5,7 +5,9 @@ import {Navigate, useNavigate, useParams } from 'react-router-dom'
 export default function Pictures () {
     return (
         <div className="pictures">
-            
+            <img src ="pics/IENI5456 - Copy (2).JPG" alt ="Mitchell plant silo with rainbow in the background" width ="600px" length ="600px"></img>
+            <img src ="pics/IMG_0999.JPG" alt ="Mitchell Plant preheater" width ="600px" length ="600px"></img>
+            <img src ="pics/IMG_1446.JPG" alt ="Mitchell plant in construction" width ="600px" length ="600px"></img>
         </div>
     )
 }
