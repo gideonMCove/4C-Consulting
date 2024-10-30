5883cca5a14fce5c03b0b836a22985d3b6ecb41e
GideonCove <gideoncove@gmail.com>
2024-10-30 14:12:33 -0400

    footer rough css done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,10 +6,10 @@ import {Navigate, useNavigate, useParams } from 'react-router-dom'
 export default function Contact () {
     return(
         <div className ="Contact">
-            <p>4See Consulting</p><br />
-            <p>307 Island Drive Lane</p><br />
-            <p>Maberly, Ontario</p><br />
-            <p>Email: Kevin.Cove@4seeconsulting.com</p><br />
+            <p>  4See Consulting    </p>
+            <p>    307 Island Drive Lane</p>
+            <p>Maberly, Ontario</p>
+            <p>Email: Kevin.Cove@4seeconsulting.com</p>
             <p>Phone: 484-632-9992</p>
         </div>
     )
