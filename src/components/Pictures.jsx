9061b115761c1b18a844a72064d22725795c70ac
GideonCove <gideoncove@gmail.com>
2024-10-30 11:24:34 -0400

    updating classes and putting them in css file

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -4,7 +4,7 @@ import {Navigate, useNavigate, useParams } from 'react-router-dom'
 
 export default function Pictures () {
     return (
-        <div className="pictures">
+        <div className="Pictures">
             <img src ="pics/IENI5456 - Copy (2).JPG" alt ="Mitchell plant silo with rainbow in the background" width ="600px" length ="600px"></img>
             <img src ="pics/IMG_0999.JPG" alt ="Mitchell Plant preheater" width ="600px" length ="600px"></img>
             <img src ="pics/IMG_1446.JPG" alt ="Mitchell plant in construction" width ="600px" length ="600px"></img>
