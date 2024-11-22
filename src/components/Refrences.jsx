fadc37f324a7b0cadf2e81bf6a442c8d447562e9
GideonCove <gideoncove@gmail.com>
2024-11-22 14:38:46 -0500

    adding references

---
+++ src/components/Refrences.jsx
@@ -0,0 +1,11 @@
+import axios from "axios"
+import { useState, useEffect} from "react"
+import {Navigate, useNavigate, useParams } from 'react-router-dom'
+
+export default function References () {
+    return (
+        <div className = "References">
+            
+        </div>
+    )
+}
